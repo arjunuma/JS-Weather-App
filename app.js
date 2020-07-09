51d0ecc1a60c185fa368d7cb7cf8fa6f38c2feee
arjunuma <umashankar.arjun@gmail.com>
2020-07-09 11:35:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function setQuery (evt) {
 }
 
 function getResults (query) {
-    fetch('${api.base}weather?q=${query}&units=metric&appid=${api.key}')
+    fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
         .then(weather => {
             return weather.json();
         }).then(displayResults);
@@ -22,12 +22,25 @@ function getResults (query) {
 
 function displayResults (weather) {
     console.log(weather);
+    if(weather.message=="city not found"){
+        alert("Please enter valid name");
+    }
     let city = document.querySelector('.location .city');
-    city.innerText = '${weather.name}, ${weather.sys.country}';
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+    //city.innerText = weather.name +""+ weather.sys.country;
 
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.status .temperature');
+    temperature.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    let weather_el = document.querySelector('.status .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let lowhi = document.querySelector('.low-hi');
+    lowhi.innerText = `${Math.round(weather.main.temp_min)}°c/ ${Math.round(weather.main.temp_max)}°c`;
 
     function dateBuilder (d){
         let months = ["January","Febuary","March","April","May","June",
@@ -40,6 +53,6 @@ function displayResults (weather) {
         let month = months[d.getMonth()];
         let year = d.getFullYear();
 
-        return '${day} ${date} ${month} ${year}';
+        return `${day} ${date} ${month} ${year}`;
     }
 }
